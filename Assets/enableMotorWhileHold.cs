eee5c9f42aff07cfe8391f659ea625ab2e88399a
Gonzako <al341522@uji.es>
2019-12-28 20:26:00 +0100

    claw numbertweaking

--- Assets/enableMotorWhileHold.cs
+++ Assets/enableMotorWhileHold.cs
@@ -4,33 +4,47 @@
  * Gonzako123@gmail.com
  *
  */
- 
+
+using System;
 using UnityEngine;
  
 public class enableMotorWhileHold : MonoBehaviour
 {
     #region Public Fields
     #endregion
- 
+
     #region Private Fields
+    private HingeJoint hj;
     #endregion
 
     #region Public Methods
+    public static Action onMotorStart;
+    public static Action onMotorEnd;
     #endregion
 
     #region Private Methods
     #endregion
 
 
-    #if false
+    #if true
     #region Unity API
 
     void Start()
     {
+        hj = GetComponent<HingeJoint>();
     }
  
-    void FixedUpdate()
+    void Update()
     {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            onMotorStart?.Invoke();
+            hj.useMotor = true;
+        } else if (Input.GetButtonUp("Fire1"))
+        {
+            onMotorEnd?.Invoke();
+            hj.useMotor = false;
+        }        
     }
 
     #endregion
