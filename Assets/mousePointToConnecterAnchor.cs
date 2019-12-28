22b39c3b03349c8aa1332da8ac6c3c69233c2822
Gonzako <al341522@uji.es>
2019-12-28 17:32:19 +0100

    started with claw following mouse code

---
+++ Assets/mousePointToConnecterAnchor.cs
@@ -0,0 +1,44 @@
+﻿/*
+ *
+ * Copyright (c) Gonzako
+ * Gonzako123@gmail.com
+ *
+ */
+ 
+using UnityEngine;
+ 
+[RequireComponent(typeof(SpringJoint))]
+public class mousePointToConnecterAnchor : MonoBehaviour
+{
+    #region Public Fields
+    #endregion
+
+    #region Private Fields
+    SpringJoint springJoint;
+    #endregion
+
+    #region Public Methods
+    #endregion
+
+    #region Private Methods
+    #endregion
+
+
+    #if true
+    #region Unity API
+
+    void Start()
+    {
+        springJoint = GetComponent<SpringJoint>();
+
+    }
+ 
+    void FixedUpdate()
+    {
+        springJoint.connectedAnchor = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    #endregion
+    #endif
+ 
+}
