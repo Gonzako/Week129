aa261b57c9c32a9f2aba64ec88ef134fa3354e96
duojet2ez <epstringskip@gmail.com>
2019-12-29 16:05:58 -0500

    Added a collision box sound

---
+++ Assets/CollidingBox.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollidingBox : MonoBehaviour
+{
+	AudioSource audiosource; 
+    void Start()
+    {
+		audiosource = GetComponent<AudioSource> (); 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void OnCollisionEnter(Collision collision)
+	{
+		audiosource.Play (); 
+	
+	}
+
+}
