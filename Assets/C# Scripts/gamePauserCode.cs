e219eff5d3253a3efb2d832df400685e1fba55a5
Gonzako <al341522@uji.es>
2019-12-29 18:46:24 +0100

    updated pause menu

---
+++ Assets/C# Scripts/gamePauserCode.cs
@@ -0,0 +1,48 @@
+﻿/*
+ *
+ * Copyright (c) Gonzako
+ * Gonzako123@gmail.com
+ *
+ */
+ 
+using UnityEngine;
+ 
+public class gamePauserCode : MonoBehaviour
+{
+    #region Public Fields
+    #endregion
+ 
+    #region Private Fields
+    #endregion
+
+    #region Public Methods
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+    #endregion
+
+    #region Private Methods
+    #endregion
+
+
+    #if false
+    #region Unity API
+
+    void Start()
+    {
+    }
+ 
+    void FixedUpdate()
+    {
+    }
+
+    #endregion
+    #endif
+ 
+}
