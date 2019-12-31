ed79bd622fb32d56d020eb33dc301868c55b31e0
duojet2ez <epstringskip@gmail.com>
2019-12-30 19:05:53 -0500

    Added claw sfx and new script attached to claw arm

---
+++ Assets/C# Scripts/ClawSound.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClawSound : MonoBehaviour
+{
+	HingeJoint claw; 
+	AudioSource audiosource;
+	AudioLowPassFilter filter; 
+
+
+    void Start()
+    {
+		claw = GetComponent<HingeJoint> (); 
+		audiosource = GetComponent<AudioSource> ();
+		filter = GetComponent<AudioLowPassFilter> (); 
+
+		filter.cutoffFrequency = 9720; 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		velocityCheck(); 
+    }
+
+	void velocityCheck()
+	{
+		if (claw.velocity < -500 || claw.velocity > 500)
+		{ 
+			audiosource.pitch = pitchFinder();  
+			audiosource.Play(); 
+		}
+	}
+
+	float pitchFinder()
+	{
+		float randomNumber = Random.Range (1.0f, 2.0f); 
+		return randomNumber; 
+	}
+
+
+}
