52ed7986659e4cbff0ea5c1c1016c3a18c978216
Gonzako <al341522@uji.es>
2019-12-31 19:02:46 +0100

    created light colour change code

---
+++ Assets/C# Scripts/lightChanger.cs
@@ -0,0 +1,52 @@
+﻿/*
+ *
+ * Copyright (c) Gonzako
+ * Gonzako123@gmail.com
+ *
+ */
+ 
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class lightChanger : MonoBehaviour
+{
+    #region Public Fields
+    [ColorUsage(true, true)]
+    public Color emissionToChangeTo;
+    public Color colorToChangeTo;
+    #endregion
+ 
+    #region Private Fields
+    #endregion
+
+    #region Public Methods
+    public void ChangeColour()
+    {
+        var renderer = GetComponent<Renderer>();
+        MaterialPropertyBlock mat = new MaterialPropertyBlock();
+        mat.SetColor("_BaseColor", colorToChangeTo);
+        mat.SetColor("_EmissionColor", emissionToChangeTo);
+        renderer.SetPropertyBlock(mat);
+    }
+    #endregion
+
+    #region Private Methods
+    #endregion
+
+
+    #if true
+    #region Unity API
+
+    void Start()
+    {
+        ChangeColour();
+    }
+ 
+    void FixedUpdate()
+    {
+    }
+
+    #endregion
+    #endif
+ 
+}
